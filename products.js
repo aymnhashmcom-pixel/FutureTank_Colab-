bb9e63d17b7a0d153ab3dabc8a63bc63e5489a65
aymnhashmcom-pixel <aymnhashm.com@gmail.com>
2026-01-07 09:51:03 +0200

    Replace local storage products with hardcoded list
    Signed-off-by: aymnhashmcom-pixel <aymnhashm.com@gmail.com>

--- products.js
+++ products.js
@@ -1,12 +1,30 @@
-const container = document.getElementById("products-container");
-const products = JSON.parse(localStorage.getItem("ft_products")) || [];
+const products = [
+  {
+    name: "خزان مياه بولي إيثيلين",
+    category: "خزانات مياه",
+    price: "12 ألف جنيه مصري",
+    image: "https://i.imgur.com/8QfQY5y.png"
+  },
+  {
+    name: "فلتر مياه 5 مراحل",
+    category: "فلاتر مياه",
+    price: "4,500 جنيه مصري",
+    image: "https://i.imgur.com/4AiXzf8.png"
+  },
+  {
+    name: "جهاز رياضي منزلي",
+    category: "أجهزة رياضية",
+    price: "8,200 جنيه مصري",
+    image: "https://i.imgur.com/1KegWPz.png"
+  }
+];
 
-container.innerHTML = "";
+const container = document.getElementById("products");
 
 products.forEach(p => {
   container.innerHTML += `
     <div class="product-card">
-      <img src="${p.image}" alt="${p.name}">
+      <img src="${p.image}">
       <h3>${p.name}</h3>
       <p>القسم: ${p.category}</p>
       <strong>${p.price}</strong>
