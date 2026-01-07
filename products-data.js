69089fb8e19ec4cad6d8a15686c423e06c6718e2
aymnhashmcom-pixel <aymnhashm.com@gmail.com>
2026-01-07 08:59:27 +0200

    Modify product prices and remove brand details
    Updated product prices to include 'مصري' and removed brand information.
    
    Signed-off-by: aymnhashmcom-pixel <aymnhashm.com@gmail.com>

--- products-data.js
+++ products-data.js
@@ -2,27 +2,26 @@ const DEFAULT_PRODUCTS = [
   {
     id: 1,
     name: "خزان مياه بولي إيثيلين",
-    brand: "FutureTank",
     category: "خزانات مياه",
-    price: "12,000 جنيه",
+    price: "12,000 جنيه مصري",
     image: "https://i.imgur.com/8QfQY5y.png"
   },
   {
     id: 2,
     name: "فلتر مياه 5 مراحل",
-    brand: "PureLife",
     category: "فلاتر مياه",
-    price: "4,500 جنيه",
+    price: "4,500 جنيه مصري",
     image: "https://i.imgur.com/4AiXzf8.png"
   },
   {
     id: 3,
     name: "جهاز رياضي منزلي",
-    brand: "FitPro",
     category: "أجهزة رياضية",
-    price: "8,200 جنيه",
+    price: "8,200 جنيه مصري",
     image: "https://i.imgur.com/1KegWPz.png"
   }
 ];
 
-localStorage.setItem("ft_products", JSON.stringify(DEFAULT_PRODUCTS));
+if (!localStorage.getItem("ft_products")) {
+  localStorage.setItem("ft_products", JSON.stringify(DEFAULT_PRODUCTS));
+}
