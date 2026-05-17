b6291d0bed271496560d3cbf488d7befc486742d
aymnhashmcom-pixel <aymnhashm.com@gmail.com>
2026-05-18 01:28:03 +0300

    Update service worker cache version and URLs
    Signed-off-by: aymnhashmcom-pixel <aymnhashm.com@gmail.com>

--- sw.js
+++ sw.js
@@ -1,18 +1,22 @@
-const CACHE_NAME = "futuretank-v1";
+const CACHE_NAME = "futuretank-v5";
 
 const urlsToCache = [
 
-"/FutureTank_Colab-/",
-"/FutureTank_Colab-/index.html",
-"/FutureTank_Colab-/style.css",
-"/FutureTank_Colab-/shop.html",
-"/FutureTank_Colab-/category.html",
-"/FutureTank_Colab-/cart.html",
-"/FutureTank_Colab-/checkout.html",
-"/FutureTank_Colab-/payment.html",
-"/FutureTank_Colab-/track-order.html"
+"./",
+"./index.html",
+"./style.css",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png",
+"./services.html",
+"./shop.html",
+"./assistant.html",
+"./portal.html",
+"./track-order.html"
 
 ];
+
+/* INSTALL */
 
 self.addEventListener(
 "install",
@@ -29,8 +33,50 @@ return cache.addAll(urlsToCache);
 
 );
 
+/* تفعيل مباشر */
+
+self.skipWaiting();
+
 }
 );
+
+/* ACTIVATE */
+
+self.addEventListener(
+"activate",
+event=>{
+
+event.waitUntil(
+
+caches.keys()
+.then(keys=>{
+
+return Promise.all(
+
+keys.map(key=>{
+
+if(key!==CACHE_NAME){
+
+return caches.delete(key);
+
+}
+
+})
+
+);
+
+})
+
+);
+
+/* تحديث مباشر */
+
+self.clients.claim();
+
+}
+);
+
+/* FETCH */
 
 self.addEventListener(
 "fetch",
@@ -38,10 +84,30 @@ event=>{
 
 event.respondWith(
 
-caches.match(event.request)
+fetch(event.request)
 .then(response=>{
 
-return response || fetch(event.request);
+const clone =
+response.clone();
+
+caches.open(CACHE_NAME)
+.then(cache=>{
+
+cache.put(
+event.request,
+clone
+);
+
+});
+
+return response;
+
+})
+.catch(()=>{
+
+return caches.match(
+event.request
+);
 
 })
 
